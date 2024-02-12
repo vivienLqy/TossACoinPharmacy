c4d1d5b1587dab10847749e0cc1cd3b0629a428c
Vivien Laquay <vivien.laquay@gmail.com>
2024-02-12 21:36:23 +0100

    slider doc

--- utilities/footer.php
+++ utilities/footer.php
@@ -1,4 +1,4 @@
 <!-- Footer-->
-<footer class=" mt-5 py-5 bg-dark">
+<footer class="bg-dark">
     <div class="container"><p class="m-0 text-center text-white">Copyright: &copy; TossACoinPharmacy 2023</p></div>
 </footer>
