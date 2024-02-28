dc673a5fb43afa19477c943b019030c38797bba8
Guy-Philippe Uberos <gpuberos.pro@gmail.com>
2024-02-28 08:59:48 +0100

    add: gitignore config + move css to assets

--- utilities/header.php
+++ utilities/header.php
@@ -23,7 +23,7 @@ require_once dirname(__DIR__) . ('/function/header.fn.php');
     <!-- Script Bootstrap -->
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <!-- Feuille de style personnalisée -->
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
     <!-- Liens vers les polices Google -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer">
